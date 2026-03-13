b7df7ec35597ab25ea18c64e7072c6faafa48d49
faraidale <frwambiwa@gmail.com>
2026-03-13 16:41:47 +0200

    Created temples.js

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+// Footer Dates
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
+
+// Hamburger Menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
